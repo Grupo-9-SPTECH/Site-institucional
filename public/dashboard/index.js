1392b7ae4085b98da2c45524be3869b02bdbc526
Leonardo Dillan <leonardo.braz@sptech.school>
2022-10-20 13:35:10 -0300

    Reparo username

--- public/dashboard/index.js
+++ public/dashboard/index.js
@@ -3,6 +3,10 @@ const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
 
+var userName = document.getElementById("userName");
+
+userName.innerHTML = sessionStorage.NOME_USUARIO;
+
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
